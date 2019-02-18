18f4e2668dfb3a3942dc46507b28b93ed79fe9b6
CagedAlchemist <37202475+CagedAlchemist@users.noreply.github.com>
2019-02-17 22:28:09 -0500

    Add files via upload

---
+++ Follow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+    static Animator animate;
+    public float enemyReach = 2f, detectDistance = 2f; //How far player is from enemy before they start walking
+    public float detectionAngle = 30;
+    private void Start()
+    {
+        animate = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (pause_menu.GameIsPaused == false)
+        {
+            Vector3 direction = player.position - this.transform.position;
+
+            if (Vector3.Distance(player.position, this.transform.position) < detectDistance)
+            {
+                direction.y = 0;
+
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+                animate.SetBool("isIdle", false);
+
+                if (direction.magnitude > enemyReach)
+                {
+                     animate.SetBool("isWalking", true);
+                     this.transform.Translate(0, 0, 0.05f);
+
+                }
+                else
+                {
+                    animate.SetBool("isWalking", false);
+                    animate.SetBool("isIdle", true);
+                    //Code here to intiate patrolling?
+                }
+            }
+        }
+    }
+}
---
+++ LightHit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LightHit : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject guard;
+    public Animator animate;
+    public Light flashlight;
+    public float playerRange = 5f;
+    void Update()
+    {
+        if (flashlight.enabled)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, playerRange))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.name == guard.name)
+                {
+                    animate.SetBool("isBlind", true);
+                }
+            }
+            
+        }
+    }
+}
